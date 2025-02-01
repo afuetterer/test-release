40101cbde89b916e4c6dc3e44692f6ed76404de1
Heinz-Alexander Fuetterer <fuetterh@posteo.de>
2025-02-01 18:04:43 +0100

    docs: dummy docs

--- src/re3data/_exceptions.py
+++ src/re3data/_exceptions.py
@@ -1,10 +1,3 @@
 # SPDX-FileCopyrightText: 2024 Heinz-Alexander Fütterer
 #
 # SPDX-License-Identifier: MIT
-
-"""The _exceptions module defines exception classes for handling error scenarios encountered when using the re3data API.
-
-Classes:
-    Re3dataError: Base exception class for errors related to the re3data API.
-    RepositoryNotFoundError: Exception raised when a repository is not found.
-"""
